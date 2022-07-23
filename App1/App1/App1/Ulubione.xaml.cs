b91306fd018e65143b1e20ee97ddaf6ae6da2ea3
Bartłomiej Kieroński <84330061+BartlomiejKieronski@users.noreply.github.com>
2022-07-23 14:17:46 +0200

    Show books

---
+++ App1/App1/App1/Ulubione.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MySqlConnector;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ulubione : ContentPage
+    {
+        mysql_connect con = new mysql_connect();
+        public IList<ulubione> Ulubiona { get; set; }
+        public class ulubione
+        {
+            //informacje
+        }
+        public Ulubione()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MySqlConnection connection = new MySqlConnection(con.connect());
+            connection.Open();
+            MySqlCommand command1 = new MySqlCommand("SELECT * FROM books", connection);
+
+        }
+    }
+}
